e8b59455f5f1aacaa08310222d7fc640e15b2994
Pietro <pietro.pustina@gmail.com>
2021-09-12 11:00:16 +0200

    Launch file completed

--- launch/launch.py
+++ launch/launch.py
@@ -5,22 +5,29 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     ld = LaunchDescription()
-    print(os.path.abspath(os.path.dirname(__file__)))
-    config = os.path.join(
-        get_package_share_directory('ros2-mocap_optitrack'),
-        )
-    print(config)
     #Create the NatNet client node
+    config = os.path.join(
+        get_package_share_directory('mocap_optitrack_client'),
+        'config',
+        'natnetclient.yaml'
+        )
     natnet_client = Node(
             package='mocap_optitrack_client',
             executable='mocap_optitrack_client',
-            name='natnet_client'
+            name='natnet_client',
+            parameters = [config]
         )
     #Create the world to base client
+    config = os.path.join(
+        get_package_share_directory('mocap_optitrack_w2b'),
+        'config',
+        'world_to_base.yaml'
+        )
     world_to_base = Node(
             package='mocap_optitrack_w2b',
             executable='mocap_optitrack_w2b',
-            name='world_to_base'
+            name='world_to_base',
+            parameters = [config]
         )
     #Add the nodes to the launch description and return it
     ld.add_action(natnet_client)
