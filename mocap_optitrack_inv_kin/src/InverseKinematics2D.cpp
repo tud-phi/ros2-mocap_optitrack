e8517864aea15f1044bd56834304451283281624
Pietro <pietro.pustina@gmail.com>
2021-10-07 14:47:39 +0200

    Implemented inverse kinematics 2D

--- mocap_optitrack_inv_kin/src/InverseKinematics2D.cpp
+++ mocap_optitrack_inv_kin/src/InverseKinematics2D.cpp
@@ -18,23 +18,25 @@ Eigen::VectorXf InverseKinematics2D::getConfiguration(const mocap_optitrack_inte
                           std::vector<double> &ds,
                           std::vector<double> &Ls) const
 {
+    //Unused paramters, just to avoid warning
+    (void) ls;
+    (void) ds;
     /*Variables initialization*/
     //Sort the rigid bodies in the message so that its all consistent with the parameters provided thrpugh the yaml file
     std::vector<mocap_optitrack_interfaces::msg::RigidBody> RBs = getSorteredBodies(msg, ring_ids);
     //
-    int i, k = 0;
+    int i;
     int nRB = (int) RBs.size();//number of rigid bodies
     //
     // Configuration vector to be returned
-    Eigen::VectorXf q(nRB*3);
+    Eigen::VectorXf q(nRB);
     //
     // Rotation matrix from ring (i) frame to ring (i-1) frame
     Eigen::Matrix3f R_i_1_ri; R_i_1_ri << 1,0,0,0,1,0,0,0,1;
-    // Position of the origin of ring (i) expressed in ring (i-1) frame
-    Eigen::Vector3f t_i_1_ri; t_i_1_ri << 0,0,0;
     //Transformation matrix from ring (i-1) to robot base frame
     Eigen::Matrix4f T_0_i_1; T_0_i_1 << 1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1;
-    double D_c_ri, delta_L_ri, Delta_x_ri, Delta_y_ri, L_factor, Delta_i, Delta_i2, ci, si, scf;
+    //Auxiliary variables for local computations
+    double qi, ci, si;
     //Epsilon quantity for limit computations, i.e. around 0
     float eps = std::numeric_limits<float>::epsilon();
     //
@@ -45,41 +47,24 @@ Eigen::VectorXf InverseKinematics2D::getConfiguration(const mocap_optitrack_inte
                                                                         RBs[i].pose_stamped.pose.orientation.y,
                                                                         RBs[i].pose_stamped.pose.orientation.z,
                                                                         RBs[i].pose_stamped.pose.orientation.w);
-        t_i_1_ri = (T_0_i_1.inverse()*(Eigen::VectorXf(4) << RBs[i].pose_stamped.pose.position.x, RBs[i].pose_stamped.pose.position.y, RBs[i].pose_stamped.pose.position.z, 1).finished()).head(3);
         /*Compute the configuration*/
         //Compute the scaling factor for the current segment
-        L_factor = Ls[i]/ls[i];
-        //Approximate 1 with 1-eps to run the limit (straight configuration)
-        if(std::abs(R_i_1_ri(2,2)) >= 1)
-        {
-            R_i_1_ri(2,2) = R_i_1_ri(2,2) + pow(10,3)*eps*((R_i_1_ri(2,2) > 0) ? -1 : 1);
-        }
-        delta_L_ri = t_i_1_ri(2)*(acos(R_i_1_ri(2,2)))/(sin(acos(R_i_1_ri(2,2))))-ls[i];
         //
-        D_c_ri = ds[i]/(ls[i]+delta_L_ri)*(pow(acos(R_i_1_ri(2,2)), 2)/(R_i_1_ri(2,2)-1));
-        Delta_x_ri = t_i_1_ri(0)*D_c_ri;
-        Delta_y_ri = t_i_1_ri(1)*D_c_ri;
-        //
-        q(k)   = L_factor*Delta_x_ri;
-        q(k+1) = L_factor*Delta_y_ri;
-        q(k+2) = L_factor*delta_L_ri;
+        q(i)   = acos(R_i_1_ri(1,1));
         //Update the transformation matrix
-        Delta_i2= pow(q(k),2) + pow(q(k+1),2);
-        //If Delta_i2 is too close to zero approximate with eps to compute the limit
-        if(std::abs(Delta_i2) < eps)
+        if(std::abs(q(i)) < eps)
         {
-            Delta_i2 = eps;
+            qi = eps;
+        }else{
+            qi    = q(i);
         }
-        Delta_i = sqrt(Delta_i2);
-        ci      = cos(Delta_i/ds[i]);
-        si      = sin(Delta_i/ds[i]);
-        scf     = ds[i]*(Ls[i]+q(k+2))/Delta_i2;
-        T_0_i_1 = T_0_i_1*(Eigen::MatrixXf(4,4) << 1+(pow(q(k),2)/Delta_i2)*(ci-1), (q(k)*q(k+1)/Delta_i2*(ci-1))    , q(k)/Delta_i*si  , scf*q(k)*(1-ci),
-                                                   (q(k)*q(k+1)/Delta_i2*(ci-1))  , 1+(pow(q(k+1),2)/Delta_i2)*(ci-1), q(k+1)/Delta_i*si, scf*q(k+1)*(1-ci),
-                                                   -q(k)/Delta_i*si               , -q(k+1)/Delta_i*si               , ci               , scf*Delta_i*si,
-                                                   0                              , 0                                , 0                , 1).finished();
-        //Update the iterator for the configuration vector
-        k += 3;
+        ci = cos(qi);
+        si = sin(qi);
+        //
+        T_0_i_1 = T_0_i_1*(Eigen::MatrixXf(4,4) << ci , -si, 0, Ls[i]*si/qi,
+                                                   si ,  ci, 0, Ls[i]*(1-ci)/qi,
+                                                     0,   0, 1, 0,
+                                                     0,   0, 0, 1).finished();
     }
     //Log the configuration vector
     RCLCPP_DEBUG(this->IKNode->get_logger(), "Configuration vector : \n");
